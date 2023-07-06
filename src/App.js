0f52e2c95ab765a20bd01f5fbc864aa5a6b351d2
Amar <amarkrishna25@gmail.com>
2023-07-06 15:25:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react'
+import {
+    FacebookIcon,
+    FacebookShareButton,
+    TwitterIcon,
+    TwitterShareButton,
+    WhatsappIcon,
+    WhatsappShareButton,
+} from 'react-share'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function getUrFromService() {
+    // The real implementation would make a network call here.
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    return 'https://via.placeholder.com/150'
 }
 
-export default App;
+export default function App() {
+    const shareButton = useRef()
+    const [url, setUrl] = useState('none') // Unfortunately, we have to have a dummy string here, or FacebookShareButton will blow up.
+
+    // Provide an onClick handler that asyncronously fetches the url and sets it in the state.
+    const onClick = async () => {
+        // Be sure to check for the "none" state, so we don't trigger an infinite loop.
+        if (url === 'none') {
+            const newUrl = await getUrFromService()
+            setUrl(newUrl)
+        }
+    }
+
+    // Whenever "url" changes and we re-render, we manually fire the click event on the button, and then re-set the url.
+    useEffect(() => {
+        if (url !== 'none') {
+            shareButton.current?.click()
+            setUrl('none')
+        }
+    }, [url, shareButton])
+
+    return (
+        <>
+            <TwitterShareButton
+                ref={shareButton}
+                // Disable calling the dialog if we don't have a url yet.
+                openShareDialogOnClick={url !== 'none'}
+                url={url}
+                onClick={onClick}
+                image="https://via.placeholder.com/150"
+            >
+                <TwitterIcon />
+            </TwitterShareButton>
+            <WhatsappShareButton
+                ref={shareButton}
+                // Disable calling the dialog if we don't have a url yet.
+                openShareDialogOnClick={url !== 'none'}
+                url={url}
+                onClick={onClick}
+                itemRef=''
+            >
+                <WhatsappIcon />
+            </WhatsappShareButton>
+        </>
+    )
+}
